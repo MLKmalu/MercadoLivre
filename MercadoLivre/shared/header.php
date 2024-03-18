b1ca1cac5130f92496de59dcecc02bb6037cf6b9
MLKmalu <marialuisa.ctism@gmail.com>
2024-03-18 20:27:04 -0300

    passei para o git

---
+++ MercadoLivre/shared/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mercado Livre</title>
+        <script src="js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="img/mercado_livre-icon.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+    
+    <!-- Cabeça da Página -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-warning fw-bold">        
+        <a class="navbar-brand mx-4" href="index.php"><img src="img/mercado_livre-icon.png" alt="Voltar para a página principal do Mercado Livre" class="img-fluid img-fluid w-2 h-2 ml-3" width="50px">  Mercado Livre</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>        
+        <div class="collapse navbar-collapse mx-4" id="navbarMain">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Perfil</a>
+            </li>
+          </ul>
+        </div>
+      </div>      
+    </nav>
+    <!-- Corpo Página -->
+<main class="m-5">
